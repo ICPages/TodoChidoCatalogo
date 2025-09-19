7688ca25725dd36be2f43965f729968a5e4136cb
ICPages <imercoriasanchez@gmail.com>
2025-09-19 03:52:16 -0600

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -1,7 +1,11 @@
-document.addEventListener("mousemove", function(e) {
-  const cursor = document.getElementById("cursor");
-  if (cursor) {
-    cursor.style.left = e.clientX + "px";
-    cursor.style.top = e.clientY + "px";
-  }
-});
+// cursor.js
+// Detectar si NO es un dispositivo móvil
+if (!/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+  document.addEventListener("mousemove", function(e) {
+    const cursor = document.getElementById("cursor");
+    if (cursor) {
+      cursor.style.left = e.clientX + "px";
+      cursor.style.top = e.clientY + "px";
+    }
+  });
+}
